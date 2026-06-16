8e30511f912dad72507d4e2d356c87c4dc8ff058
mved11 <vedris.mateo@icloud.com>
2026-06-16 08:12:24 +0200

    Add connect-four to local test mode

--- src/games/gameRegistry.ts
+++ src/games/gameRegistry.ts
@@ -2,7 +2,7 @@ import { ticTacToeModule } from "./tic-tac-toe";
 import { connectFourModule } from "./connect-four";
 import type { BoardGameModule } from "./types";
 
-export const gameRegistry: BoardGameModule[] = [ticTacToeModule];
+export const gameRegistry: BoardGameModule[] = [ticTacToeModule, connectFourModule];
 
 export function getGameById(gameId: string): BoardGameModule | undefined {
   return gameRegistry.find((game) => game.id === gameId);
